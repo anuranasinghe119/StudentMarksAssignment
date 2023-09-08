2a79cc2bd3ce0798574f67231bfba077a6f9a6eb
Anupa Ranasinghe <anuranasinghe119@gmail.com>
2023-09-08 20:19:48 +0530

    F2 Completed - To input marks from 30 students and validations done

--- AppCode/Main.java
+++ AppCode/Main.java
@@ -2,20 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     private static final int STUDENTS = 30;
-    private static double[] studentMarks = new double[STUDENTS];
-
+    static double[] studentMarks = new double[STUDENTS];
+    private  static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        RunApplication();
+        StartApplication();
+        InputMarks();
     }
 
-    public static void RunApplication(){
-        Scanner scanner = new Scanner(System.in);
+    public static void StartApplication(){
         System.out.println("Please enter assignment name :");
         String assignmentName = scanner.next();
     }
 
-    public static void GetMarks(){
-        
+    public static void InputMarks(){
+        for (int i = 0; i < STUDENTS; i++) {
+            System.out.print("Enter marks of Student " + i +" : "  );
+            String studentMarkStr = scanner.next();
+            try {
+                double stdMark = Double.parseDouble(studentMarkStr);
+                if (stdMark <= 30 && stdMark > 0)
+                    studentMarks[i] = stdMark;
+                else {
+                    System.err.println("Number "+stdMark+" is out of Range.\nPlease enter number within valid" +
+                            "Range 0 - 30");
+                    i--;
+                    System.out.println("\n");
+                }
+            }
+            catch (Exception e){
+                System.err.print("Unable to convert number :" + e.getMessage());
+                i--;
+                System.out.println("\n");
+            }
+
+        }
     }
 
 }
