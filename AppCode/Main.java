0d01c6c3f5e3b9f23b019b0ea4777375fc4bd657
Anupa Ranasinghe <anuranasinghe119@gmail.com>
2023-09-08 19:52:25 +0530

    Added initial files, Implemented functionality 1 to input assignment

---
+++ AppCode/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final int STUDENTS = 30;
+    private static double[] studentMarks = new double[STUDENTS];
+
+    public static void main(String[] args) {
+        RunApplication();
+    }
+
+    public static void RunApplication(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter assignment name :");
+        String assignmentName = scanner.next();
+    }
+
+    public static void GetMarks(){
+        
+    }
+
+}
