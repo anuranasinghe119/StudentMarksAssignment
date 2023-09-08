1f82a0faeece3c7fa3bf4da33f1dd93089cfa24a
Anupa Ranasinghe <anuranasinghe119@gmail.com>
2023-09-08 20:26:29 +0530

    Format code

--- AppCode/Main.java
+++ AppCode/Main.java
@@ -2,39 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     private static final int STUDENTS = 30;
+    private static final Scanner scanner = new Scanner(System.in);
     static double[] studentMarks = new double[STUDENTS];
-    private  static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         StartApplication();
         InputMarks();
     }
 
-    public static void StartApplication(){
+    public static void StartApplication() {
         System.out.println("Please enter assignment name :");
         String assignmentName = scanner.next();
     }
 
-    public static void InputMarks(){
+    public static void InputMarks() {
         for (int i = 0; i < STUDENTS; i++) {
-            System.out.print("Enter marks of Student " + i +" : "  );
+            System.out.print("Enter marks of Student " + i + " : ");
             String studentMarkStr = scanner.next();
             try {
                 double stdMark = Double.parseDouble(studentMarkStr);
                 if (stdMark <= 30 && stdMark > 0)
                     studentMarks[i] = stdMark;
                 else {
-                    System.err.println("Number "+stdMark+" is out of Range.\nPlease enter number within valid" +
+                    System.err.println("Number " + stdMark + " is out of Range.\nPlease enter number within valid" +
                             "Range 0 - 30");
                     i--;
                     System.out.println("\n");
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.err.print("Unable to convert number :" + e.getMessage());
                 i--;
                 System.out.println("\n");
             }
-
         }
     }
 
